881695356670d18232afb824bda4be024bace025
alicia <apressley@clearviewaudit.com>
2016-04-29 13:19:42 -0500

    Highlight selected provider

--- client/main.js
+++ client/main.js
@@ -4,5 +4,22 @@ Template.provider_list.onRendered = function(){
 Template.provider_list.helpers({
     provider: function() {
         return Providers.find();
+    },
+    'selected': function(){
+        var provider_id= this._id;
+        //console.log(provider_id);
+        var selected_provider = Session.get('selected_provider');
+        if(provider_id == selected_provider){
+            return "selected"
+        }
+    }
+});
+Template.provider_list.events({
+    'click .provider_listed': function(){
+        var provider_id = this._id;
+        Session.set('selected_provider', provider_id);
+        Session.get('selected_provider');
+        var selected_provider = Session.get('selected_provider');
+        console.log(selected_provider);
     }
 });
