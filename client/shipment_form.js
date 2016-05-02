b4783ea88fa9503875b7b6417f36ae3e2621d652
alicia <apressley@clearviewaudit.com>
2016-05-02 16:32:40 -0500

    WIP- demo shipment button

--- client/shipment_form.js
+++ client/shipment_form.js
@@ -17,11 +17,15 @@ Template.shipment_form.events({
         autoform_generated_document_object = AutoForm.getFormValues('insert_shipment_form').insertDoc
         //autoform_generated_document_object.provider_id = {}
         autoform_generated_document_object.createdBy = Meteor.userId()
-        console.log("createdby " + autoform_generated_document_object.createdBy)
+        console.log("createdBy " + autoform_generated_document_object.createdBy)
         Session.set('shipment_ready', autoform_generated_document_object);
         Session.get('shipment_ready');
         sAlert.success("Your shipment has been submitted! Your providers will be available next.");
         var ready = Session.get('shipment_ready')
         return console.log(ready);
+    },
+    'demo': function(event){
+        autoform_inserted_doc_object = AutoForm.get
+
     }
 });
