c76c6d45c979a499745e463100ef8ce88a61f3fb
alicia <apressley@clearviewaudit.com>
2016-05-02 13:39:49 -0500

    added createdBy field to shipments

--- client/shipment_form.js
+++ client/shipment_form.js
@@ -1,20 +1,20 @@
 Template.shipment_form.onRendered
-Meteor.subscribe("shipments");
+    Meteor.subscribe("shipments");
 
 Template.shipment_form.helpers({
     'shipment_count': function(){
         var currentUserId = Meteor.userId();
         return Shipments.find().count()
     }
-
 });
+
 Template.shipment_form.events({
-    'submit': function(event, tmpl) {
+    'submit': function(event) {
         event.preventDefault();
         autoform_generated_document_object = AutoForm.getFormValues('insert_shipment_form').insertDoc
         //autoform_generated_document_object.provider_id = {}
         autoform_generated_document_object.createdBy = Meteor.userId()
-
+        console.log("createdby " + autoform_generated_document_object.createdBy)
         Session.set('shipment_ready', autoform_generated_document_object);
         Session.get('shipment_ready');
         var ready = Session.get('shipment_ready')
